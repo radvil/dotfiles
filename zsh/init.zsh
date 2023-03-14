f428ce663b625cfd682e27b8eabf51c1bd3cfb91
Radvil <radvil@access-mobile.com>
2023-03-15 01:41:32 +0800

    commentaries

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -1,7 +1,9 @@
 #NOTE: These should run in order!
 
+# requirement utils
 source "$DOTFILES/common/funcs.sh"
 
+# source config
 source_file "$HOME/.cargo/env"
 source_file "$HOME/.nvm/nvm.sh"
 source_file "$HOME/.nvm/bash_completion"
@@ -15,4 +17,5 @@ source_file "$DOTFILES/git/funcs.sh"
 source_file "$DOTFILES/lf/funcs.sh"
 source_file "$DOTFILES/alias/init.zsh"
 
+# append path to global
 PATH="$DOTFILES/bin:$PATH"
