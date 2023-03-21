0c8ab4598d11a0650ca238f6651f7b3edfa6321e
Radvil <radvil@access-mobile.com>
2023-03-22 01:36:11 +0800

    Remove unused, + Add batcat

--- nnn/init.zsh
+++ nnn/init.zsh
@@ -1,4 +1,4 @@
 export NNN_TRASH=1
 export NNN_COLORS="2136"
-export NNN_PLUG="f:fzopen;p:preview-tui;e:-!sudo -E nvim '$nnn'*;g:-!lazygit;"
+export NNN_PLUG="f:fzopen;v:preview-tui;e:-!sudo -E nvim '$nnn'*;g:-!lazygit;p:mocq;s:kdeconnect;z:autojump;"
 export NNN_FIFO="/tmp/nnn.fifo"
