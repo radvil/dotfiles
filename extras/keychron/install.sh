f438c316128ad94ca4e08847ebf8704e25fd2ecf
Radvil <radvil@@access-mobile.com>
2023-04-02 22:05:03 +0800

    reset systemd user config

--- extras/keychron/install.sh
+++ extras/keychron/install.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+source "$DOTFILES/build/utils.sh"
+
 source="$DOTFILES/extras/keychron/keychron.service" 
 destination="/etc/systemd/system/keychron.service"
 if [[ ! -f "$destination" ]]; then
   sudo ln -sf "$source" "$destination"
-  sudo systemctl enable keychron.service
+  sudo systemctl --user enable keychron.service
   okay "\"Keychron Service\" successfully setup!"
 else
   info "\"Keychron Service\" has already exists. Skipping..."
