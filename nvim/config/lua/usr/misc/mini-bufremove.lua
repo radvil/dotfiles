4ad524bb68a60ae3fec7faafa5bc26e475e45de7
Radvil <radvil@@access-mobile.com>
2023-06-09 03:22:01 +0800

    fix keymapping

--- nvim/config/lua/usr/misc/mini-bufremove.lua
+++ nvim/config/lua/usr/misc/mini-bufremove.lua
@@ -2,55 +2,52 @@
 local M = {}
 M[1] = "echasnovski/mini.bufremove"
 
-local function remove(force, bufnr)
-  force = force == nil and false
-  bufnr = bufnr == nil and 0
-  require("mini.bufremove").delete(bufnr or 0, force or false)
-end
-
 M.init = function()
   vim.api.nvim_create_user_command("BD", function()
-    remove()
-  end, { desc = "Remove buffer [safe]" })
+    require("mini.bufremove").delete(0, false)
+  end, { desc = "Delete current buffer" })
+
   vim.api.nvim_create_user_command("BAD", function()
     for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
       if vim.api.nvim_buf_is_loaded(bufnr) then
-        remove(false, bufnr)
+        require("mini.bufremove").delete(bufnr, false)
       end
     end
-  end, { desc = "Remove all buffers [safe]" })
+  end, { desc = "Delte all buffers" })
+
   vim.api.nvim_create_user_command("BF", function()
-    remove(true)
-  end, { desc = "Remove buffer [force]" })
+    require("mini.bufremove").delete(0, true)
+  end, { desc = "Delete current buffer (force)" })
+
   vim.api.nvim_create_user_command("BAF", function()
     for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
       if vim.api.nvim_buf_is_loaded(bufnr) then
-        remove(true, bufnr)
+        require("mini.bufremove").delete(bufnr, true)
       end
     end
-  end, { desc = "Remove all buffers [force]" })
+  end, { desc = "Remove all buffers (force)" })
 end
 
 M.keys = {
   {
     "<Leader>bd",
     "<Cmd>BD<Cr>",
-    desc = "buffer » safe delete",
+    desc = "buffer » delete",
   },
   {
     "<Leader>bf",
     "<Cmd>BF<Cr>",
-    desc = "buffer » force delete",
+    desc = "buffer » delete (force)",
   },
   {
     "<Leader>bD",
     "<Cmd>BAD<Cr>",
-    desc = "buffer » safe delete (all)",
+    desc = "buffer » delete all",
   },
   {
     "<Leader>bF",
     "<Cmd>BAF<Cr>",
-    desc = "buffer » force delete (all)",
+    desc = "buffer » delete all (force)",
   },
 }
 
