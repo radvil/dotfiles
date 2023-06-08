7bc4305c02a504efca80722cffaf15f6948147c5
Radvil <radvil@@access-mobile.com>
2023-06-09 01:28:37 +0800

    disable fokus also remove zenmode keymap

--- nvim/config/lua/usr/ui/fokus-nvim.lua
+++ nvim/config/lua/usr/ui/fokus-nvim.lua
@@ -4,14 +4,6 @@ M[1] = "radvil/fokus.nvim"
 M.enabled = false
 M.event = "BufReadPre"
 M.dependencies = { "folke/twilight.nvim", "folke/zen-mode.nvim" }
----@type FokusOptions
-M.keys = {
-  {
-    "<Leader>wf",
-    "<Cmd>ZenMode<Cr>",
-    desc = "fokus-nvim » toggle zen mode",
-  },
-}
 M.opts = {
   view = {
     notify_status_change = false,
