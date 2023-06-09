75908b66ad0b43112d4d886b56169b7e2dfd1932
Radvil <radvil@@access-mobile.com>
2023-06-10 05:51:19 +0800

    lock

--- nvim/config/lua/usr/lsp/init.lua
+++ nvim/config/lua/usr/lsp/init.lua
@@ -1,7 +1,8 @@
 return {
   require("usr.lsp.mason"),
-  require("usr.lsp.lspconfig"),
   require("usr.lsp.null-ls"),
+  require("usr.lsp.lspconfig"),
   require("usr.lsp.lang.typescript"),
   require("usr.lsp.lang.angularls").treesitter,
+  require("usr.lsp.lang.go"),
 }
