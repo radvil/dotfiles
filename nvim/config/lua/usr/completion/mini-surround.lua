65202c408ffb648b8edf277cf5932851f6a57ce6
Radvil <radvil@@access-mobile.com>
2023-06-10 01:02:41 +0800

    change mapping for find prev/next surroundings

--- nvim/config/lua/usr/completion/mini-surround.lua
+++ nvim/config/lua/usr/completion/mini-surround.lua
@@ -1,4 +1,3 @@
----@desc surround
 ---@type LazySpec
 local M = {}
 M[1] = "echasnovski/mini.surround"
@@ -6,28 +5,31 @@ M.opts = {
   mappings = {
     add = "so",
     delete = "sd",
-    find = "sn",
-    find_left = "sp",
+    find_left = "s[",
+    find = "s]",
     highlight = "sh",
     replace = "ss",
     update_n_lines = "",
   },
 }
----@diagnostic disable-next-line: assign-type-mismatch
-M.keys = function(plugin, keys)
-  local opts = require("lazy.core.plugin").values(plugin, "opts", false)
-  local mode = { "n", "x" }
-  local mappings = {
-    { opts.mappings.add,       desc = "Open/Add",               mode = mode },
-    { opts.mappings.delete,    desc = "Delete",                 mode = mode },
-    { opts.mappings.find,      desc = "Find Next",              mode = mode },
-    { opts.mappings.find_left, desc = "Find Prev",              mode = mode },
-    { opts.mappings.replace,   desc = "Replace/Subtitute",      mode = mode },
-    { opts.mappings.highlight, desc = "Highlight Under Cursor", mode = mode },
-  }
-  return vim.list_extend(mappings, keys)
-end
+
+-- ---@diagnostic disable-next-line: assign-type-mismatch
+-- M.keys = function(plugin, keys)
+--   local opts = require("lazy.core.plugin").values(plugin, "opts", false)
+--   local mode = { "n", "x" }
+--   local mappings = {
+--     { opts.mappings.add,       desc = "Surround » open/add",          mode = mode },
+--     { opts.mappings.delete,    desc = "Surround » delete",            mode = mode },
+--     { opts.mappings.find,      desc = "Surround » find next",         mode = mode },
+--     { opts.mappings.find_left, desc = "Surround » find prev",         mode = mode },
+--     { opts.mappings.replace,   desc = "Surround » replace/subtitute", mode = mode },
+--     { opts.mappings.highlight, desc = "Surround » highlight current", mode = mode },
+--   }
+--   return vim.list_extend(mappings, keys)
+-- end
+
 M.config = function(_, opts)
   require("mini.surround").setup(opts)
 end
+
 return M
