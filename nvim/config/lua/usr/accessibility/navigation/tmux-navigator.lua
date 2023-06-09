d1a3cca8407fa1a7ffce26972ef4b0b82258aadd
Radvil <radvil@@access-mobile.com>
2023-06-10 01:01:34 +0800

    specificly replace keymaps for whichkey compatibility

--- nvim/config/lua/usr/accessibility/navigation/tmux-navigator.lua
+++ nvim/config/lua/usr/accessibility/navigation/tmux-navigator.lua
@@ -2,4 +2,11 @@
 local M = {}
 M[1] = "christoomey/vim-tmux-navigator"
 M.enabled = os.getenv("TMUX") ~= nil
+M.init = function()
+  vim.cmd [[let g:tmux_navigator_no_mappings = 1]]
+  Map("n", "<c-h>", ":TmuxNavigateLeft<cr>", { desc = "Window/Tmux » navigate left" })
+  Map("n", "<c-j>", ":TmuxNavigateDown<cr>", { desc = "Window/Tmux » navigate down" })
+  Map("n", "<c-k>", ":TmuxNavigateUp<cr>", { desc = "Window/Tmux » navigate up" })
+  Map("n", "<c-l>", ":TmuxNavigateRight<cr>", { desc = "Window/Tmux » navigate right" })
+end
 return M
