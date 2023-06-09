b17959234943f0baad0079bca583d09d30d3ee73
Radvil <radvil@@access-mobile.com>
2023-06-10 06:43:12 +0800

    emmetls into its own file

---
+++ nvim/config/lua/usr/lsp/lang/emmet.lua
@@ -0,0 +1,17 @@
+---@type LazySpec[]
+return {
+  "neovim/nvim-lspconfig",
+  ---@type RvimLspOptions
+  opts = {
+    servers = {
+      emmet_ls = {
+        single_file_support = true,
+        on_attach = function(client, buffer)
+          client.server_capabilities.documentFormattingProvider = false
+          client.server_capabilities.documentRangeFormattingProvider = false
+          require("usr.lsp.common.utils").on_attach(client, buffer)
+        end
+      }
+    },
+  },
+}
