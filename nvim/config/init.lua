b42ea1bed0ae9b26ba16409cf6a51c14d41383be
Radvil <radvil@@access-mobile.com>
2023-06-11 02:16:07 +0800

    change bootstrap implementation

--- nvim/config/init.lua
+++ nvim/config/init.lua
@@ -1,3 +1,27 @@
-require("var")
-require("main")
-rnv.api.log("CONFIG LOADED!", "INIT")
+require("core").bootstrap({
+  -- register global variable(s) and api(s)
+  settings = require("var"),
+
+  -- load all plugins's specs
+  on_init = function(options)
+    rnv.api.log("ON INIT", "init")
+    require("lazy").setup({
+      "folke/lazy.nvim",
+      require("usr.accessibility"),
+      require("usr.colorscheme"),
+      require("usr.completion"),
+      require("usr._preview"),
+      require("usr.misc"),
+      require("usr.lsp"),
+      require("usr.ui"),
+    }, options)
+  end,
+
+  -- load these after all plugins
+  after_init = function()
+    rnv.api.log("AFTER INIT", "init")
+    require("opt.options")
+    require("opt.autocmds")
+    require("opt.keymaps")
+  end
+})
