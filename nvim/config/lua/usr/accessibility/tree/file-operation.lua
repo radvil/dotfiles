1bf5bb15f434886996246a543e3d8b0d4ea29f1a
Radvil <radvil@@access-mobile.com>
2023-06-11 05:23:22 +0800

    remove unused file-operation deps

--- nvim/config/lua/usr/accessibility/tree/file-operation.lua
+++
@@ -1,9 +0,0 @@
----@type LazySpec
-local M = {}
-M[1] = "radvil2/nvim-tree-lsp-file-operations"
-M.enabled = false
-M.dependencies = {
-  "nvim-lua/plenary.nvim",
-  "nvim-tree/nvim-tree.lua",
-}
-return M
--- nvim/config/lua/usr/accessibility/tree/init.lua
+++ nvim/config/lua/usr/accessibility/tree/init.lua
@@ -1,6 +1,5 @@
 ---@type LazySpec[]
 return {
-  require("usr.accessibility.tree.file-operation"),
   require("usr.accessibility.tree.symbols-outline"),
   require("usr.accessibility.tree.nvim-tree"),
   require("usr.accessibility.tree.neotree"),
