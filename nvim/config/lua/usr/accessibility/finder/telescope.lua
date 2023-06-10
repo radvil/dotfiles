af9713050c60402cb0881bc9b125056df29d8941
Radvil <radvil@@access-mobile.com>
2023-06-11 02:10:13 +0800

    telescope config
    - adjust keymaps
    - adjust dotfiles autocommand

--- nvim/config/lua/usr/accessibility/finder/telescope.lua
+++ nvim/config/lua/usr/accessibility/finder/telescope.lua
@@ -4,6 +4,7 @@ local util = require("utils")
 M[1] = "nvim-telescope/telescope.nvim"
 M.enabled = true
 M.cmd = "Telescope"
+
 M.dependencies = {
   "nvim-telescope/telescope-fzf-native.nvim",
   build = "make",
@@ -11,52 +12,57 @@ M.dependencies = {
     require("telescope").load_extension("fzf")
   end,
 }
+
 M.keys = {
   {
-    "<C-p>",
+    "<c-p>",
     util.telescope("files"),
-    "<Cmd>Telescope find_files<Cr>",
+    ":Telescope find_files<cr>",
     desc = "telescope » find files",
     mode = "n",
   },
   {
-    "<Leader>/d",
+    "<leader>/d",
     ":Dotfiles<cr>",
     desc = "telescope » open nvim config",
   },
   {
-    "<Leader>/f",
-    "<Cmd>Telescope find_files<cr>",
+    "<leader>/f",
+    ":Telescope find_files<cr>",
     desc = "telescope » find files (root)",
   },
-  { "<Leader>/w", util.telescope("live_grep"),                    desc = "telescope » live grep (cwd)" },
-  { "<Leader>/W", util.telescope("live_grep", { cwd = false }),   desc = "telescope » live grep (root)" },
-  { "<Leader>/S", util.telescope("grep_string"),                  desc = "telescope » grep Word (cwd)" },
-  { "<Leader>/s", util.telescope("grep_string", { cwd = false }), desc = "telescope » grep Word (root)" },
-  { "<Leader>//", "<cmd>Telescope resume<cr>",                    desc = "telescope » resume" },
+  { "<leader>/w", util.telescope("live_grep"),                    desc = "telescope » live grep (cwd)" },
+  { "<leader>/W", util.telescope("live_grep", { cwd = false }),   desc = "telescope » live grep (root)" },
+  { "<leader>/S", util.telescope("grep_string"),                  desc = "telescope » grep Word (cwd)" },
+  { "<leader>/s", util.telescope("grep_string", { cwd = false }), desc = "telescope » grep Word (root)" },
   {
-    "<Leader>/:",
-    "<Cmd>Telescope command_history<Cr>",
+    "<leader>//",
+    ":Telescope resume<cr>",
+    desc = "telescope » resume"
+  },
+  {
+    "<leader>/:",
+    ":Telescope command_history<Cr>",
     desc = "telescope » command history",
   },
   {
-    "<Leader>/b",
-    "<Cmd>Telescope buffers<Cr>",
+    "<leader>/b",
+    ":Telescope buffers<Cr>",
     desc = "telescope » find opened buffers",
   },
   {
-    "<Leader>/o",
+    "<leader>/o",
     util.telescope("oldfiles", { cwd = vim.loop.cwd() }),
     desc = "telescope » find recent files (cwd)",
   },
   {
-    "<Leader>/O",
+    "<leader>/O",
     ":Telescope oldfiles<cr>",
     desc = "telescope » find recent files (root)",
   },
   {
-    "<Leader>/h",
-    "<Cmd>Telescope help_tags<Cr>",
+    "<leader>/h",
+    ":Telescope help_tags<Cr>",
     desc = "telescope » find help tags",
   },
   {
@@ -65,37 +71,37 @@ M.keys = {
     desc = "telescope » find help tags",
   },
   {
-    "<Leader>/H",
-    "<Cmd>Telescope highlights<Cr>",
+    "<leader>/H",
+    ":Telescope highlights<Cr>",
     desc = "telescope » find highlights",
   },
   {
-    "<Leader>/k",
-    "<Cmd>Telescope keymaps<Cr>",
+    "<leader>/k",
+    ":Telescope keymaps<Cr>",
     desc = "telescope » find keymaps",
   },
   {
-    "<Leader>/M",
-    "<Cmd>Telescope man_pages<Cr>",
+    "<leader>/M",
+    ":Telescope man_pages<Cr>",
     desc = "telescope » find man pages",
   },
   {
-    "<Leader>/C",
+    "<leader>/C",
     util.telescope("colorscheme", { enable_preview = true }),
     desc = "telescope » find colorscheme",
   },
   {
-    "<Leader>/c",
-    "<Cmd>Telescope commands<Cr>",
+    "<leader>/c",
+    ":Telescope commands<Cr>",
     desc = "telescope » find available commands",
   },
   {
-    "<Leader>/X",
+    "<leader>/X",
     ":Telescope diagnostics<cr>",
     desc = "telescope » workspace diagnostics",
   },
   {
-    "<Leader>/x",
+    "<leader>/x",
     ":Telescope diagnostics bufnr=0<cr>",
     desc = "telescope » find diagnostics (cwd)",
   },
@@ -111,23 +117,23 @@ M.opts = function()
       prompt_prefix = " 🔭 ",
       mappings = {
         ["i"] = {
-          ["<A-Space>"] = actions.close,
-          ["<CR>"] = actions.select_default,
-          ["<C-h>"] = actions.select_horizontal,
-          ["<C-v>"] = actions.select_vertical,
-          ["<C-p>"] = actions.move_selection_previous,
-          ["<A-j>"] = actions.cycle_history_next,
-          ["<A-k>"] = actions.cycle_history_prev,
-          ["<C-n>"] = actions.move_selection_next,
-          ["<C-t>"] = require("trouble.providers.telescope").open_with_trouble,
-          ["<C-d>"] = actions.preview_scrolling_down,
-          ["<C-u>"] = actions.preview_scrolling_up,
+          ["<a-space>"] = actions.close,
+          ["<cr>"] = actions.select_default,
+          ["<c-h>"] = actions.select_horizontal,
+          ["<c-v>"] = actions.select_vertical,
+          ["<c-p>"] = actions.move_selection_previous,
+          ["<a-j>"] = actions.cycle_history_next,
+          ["<a-k>"] = actions.cycle_history_prev,
+          ["<c-n>"] = actions.move_selection_next,
+          ["<c-t>"] = require("trouble.providers.telescope").open_with_trouble,
+          ["<c-d>"] = actions.preview_scrolling_down,
+          ["<c-u>"] = actions.preview_scrolling_up,
         },
         ["n"] = {
-          ["<C-p>"] = actions.move_selection_previous,
-          ["<C-n>"] = actions.move_selection_next,
-          ["<C-h>"] = actions.select_horizontal,
-          ["<C-v>"] = actions.select_vertical,
+          ["<c-p>"] = actions.move_selection_previous,
+          ["<c-n>"] = actions.move_selection_next,
+          ["<c-h>"] = actions.select_horizontal,
+          ["<c-v>"] = actions.select_vertical,
         },
       },
     },
@@ -137,7 +143,8 @@ end
 function M.init()
   vim.api.nvim_create_user_command("Dotfiles", function()
     require("utils").telescope("files", {
-      cwd = rnv.path.config,
+      prompt_title = '🔧 DOTFILES',
+      cwd = os.getenv("DOTFILES"),
     })()
   end, {})
 end
