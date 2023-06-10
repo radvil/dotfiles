81e4f4e86c2d479f85b6bff187f2897b68ef1455
Radvil <radvil@@access-mobile.com>
2023-06-11 02:11:59 +0800

    change options from object to func

--- nvim/config/lua/usr/colorscheme/catppuccin.lua
+++ nvim/config/lua/usr/colorscheme/catppuccin.lua
@@ -3,88 +3,88 @@ local M = {}
 M[1] = "catppuccin/nvim"
 M.name = "catppuccin"
 
-M.opts = {
-  ---@type "latte" | "frappe" | "macchiato" | "mocha"
-  flavour = rnv.opt.colorvariant,
-  transparent_background = rnv.opt.transbg,
-  term_colors = true,
-  dim_inactive = {
-    enabled = false,
-    percentage = 0.15,
-    shade = "dark",
-  },
-  background = {
-    light = "latte",
-    dark = "mocha",
-  },
-  styles = {
-    conditionals = { "italic" },
-    comments = { "italic" },
-  },
-  integrations = {
-    treesitter = true,
-    coc_nvim = false,
-    lsp_trouble = true,
-    cmp = true,
-    lsp_saga = false,
-    leap = false,
-    gitgutter = false,
-    gitsigns = true,
-    telescope = true,
-    which_key = true,
-    dashboard = false,
-    neogit = false,
-    vim_sneak = false,
-    fern = false,
-    barbar = false,
-    bufferline = true,
-    markdown = true,
-    lightspeed = false,
-    ts_rainbow = false,
-    hop = true,
-    notify = true,
-    telekasten = false,
-    symbols_outline = false,
-    mini = false,
-    navic = false,
-    noice = false,
-    fidget = false,
-    neotree = true,
-    nvimtree = {
+M.opts = function()
+  return {
+    ---@type "latte" | "frappe" | "macchiato" | "mocha"
+    flavour = rnv.opt.colorvariant,
+    transparent_background = rnv.opt.transbg,
+    term_colors = true,
+    dim_inactive = {
       enabled = false,
-      transparent_panel = rnv.opt.transbg,
-      show_root = true,
+      percentage = 0.15,
+      shade = "dark",
     },
-    dap = {
-      enabled = false,
-      enable_ui = false,
+    background = {
+      light = "latte",
+      dark = "mocha",
     },
-    indent_blankline = {
-      enabled = false,
-      colored_indent_levels = false,
+    styles = {
+      conditionals = { "italic" },
+      comments = { "italic" },
     },
-    native_lsp = {
-      enabled = true,
-      virtual_text = {
-        information = { "italic" },
-        warnings = { "italic" },
-        errors = { "italic" },
-        hints = { "italic" },
+    integrations = {
+      treesitter = true,
+      coc_nvim = false,
+      lsp_trouble = true,
+      cmp = true,
+      lsp_saga = false,
+      leap = false,
+      gitgutter = false,
+      gitsigns = true,
+      telescope = true,
+      which_key = true,
+      dashboard = false,
+      neogit = false,
+      vim_sneak = false,
+      fern = false,
+      barbar = false,
+      bufferline = true,
+      markdown = true,
+      lightspeed = false,
+      ts_rainbow = false,
+      hop = true,
+      notify = true,
+      telekasten = false,
+      symbols_outline = false,
+      mini = false,
+      navic = false,
+      noice = false,
+      fidget = false,
+      neotree = true,
+      nvimtree = {
+        enabled = false,
+        transparent_panel = rnv.opt.transbg,
+        show_root = true,
       },
-      underlines = {
-        information = { "underline" },
-        warnings = { "underline" },
-        errors = { "underline" },
-        hints = { "underline" },
+      dap = {
+        enabled = false,
+        enable_ui = false,
+      },
+      indent_blankline = {
+        enabled = false,
+        colored_indent_levels = false,
+      },
+      native_lsp = {
+        enabled = true,
+        virtual_text = {
+          information = { "italic" },
+          warnings = { "italic" },
+          errors = { "italic" },
+          hints = { "italic" },
+        },
+        underlines = {
+          information = { "underline" },
+          warnings = { "underline" },
+          errors = { "underline" },
+          hints = { "underline" },
+        },
       },
     },
-  },
-}
+  }
+end
 
-if rnv.opt.colorscheme == "catppuccin" then
-  M.priority = 999
-  M.lazy = false
-  M.init = function()
+M.init = function()
+  if rnv.opt.colorscheme == "catppuccin" then
     vim.cmd("colorscheme catppuccin")
   end
 end
