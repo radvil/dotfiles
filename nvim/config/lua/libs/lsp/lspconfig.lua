becdadc336abba45c109e976c62d7bc8a375776d
Radvil <radvil@@access-mobile.com>
2023-06-11 09:13:32 +0800

    move usr/lsp to libs/lsp

---
+++ nvim/config/lua/libs/lsp/lspconfig.lua
@@ -0,0 +1,115 @@
+---@type LazySpec
+local M = {}
+M[1] = "neovim/nvim-lspconfig"
+M.enabled = true
+M.event = { "BufReadPre", "BufNewFile" }
+
+M.dependencies = {
+  "mason.nvim",
+  "williamboman/mason-lspconfig.nvim",
+  {
+    "folke/neoconf.nvim",
+    cmd = "Neoconf",
+    config = true,
+  },
+  {
+    "folke/neodev.nvim",
+    opts = {
+      experimental = {
+        pathStrict = true,
+      },
+    },
+  },
+  {
+    "hrsh7th/cmp-nvim-lsp",
+    cond = function()
+      return require("common.utils").has("nvim-cmp")
+    end,
+  },
+}
+
+---@class RvnLspOptions
+M.opts = {
+  install_missing_servers = true,
+  ---NOTE: set value to false to prevent autoinstall servers
+  servers = {
+    ["jsonls"] = true,
+    ["bashls"] = true,
+    ["html"] = {},
+    ["cssls"] = {},
+    ["lua_ls"] = {
+      settings = {
+        Lua = {
+          workspace = {
+            checkThirdParty = false,
+          },
+          completion = {
+            callSnippet = "Replace",
+          },
+        },
+      },
+    },
+    ["tsserver"] = {
+      settings = {
+        typescript = {
+          format = {
+            indentSize = vim.o.shiftwidth,
+            convertTabsToSpaces = vim.o.expandtab,
+            tabSize = vim.o.tabstop,
+          },
+        },
+        javascript = {
+          format = {
+            indentSize = vim.o.shiftwidth,
+            convertTabsToSpaces = vim.o.expandtab,
+            tabSize = vim.o.tabstop,
+          },
+        },
+        completions = {
+          completeFunctionCalls = true,
+        },
+      },
+    },
+  },
+}
+
+local function get_defaults()
+  return {
+    capabilities = require("common.lsp").make_client_capabilities(),
+    on_attach = require("common.lsp").default_on_attach,
+  }
+end
+
+---@param options RvnLspOptions
+local setup_language_servers = function(options)
+  local ensure_installed = {}
+  ---@param opts function | table | boolean
+  for server, opts in pairs(options.servers) do
+    if type(opts) == "boolean" and opts == false then
+      opts = get_defaults()
+    else
+      ensure_installed[#ensure_installed + 1] = server
+      if type(opts) == "function" then
+        opts = opts() or get_defaults()
+      elseif type(opts) == "table" then
+        opts = vim.tbl_deep_extend("force", get_defaults(), opts or {})
+      else
+        opts = get_defaults()
+      end
+    end
+    require("lspconfig")[server].setup(opts)
+  end
+  require("mason-lspconfig").setup({
+    automatic_installation = options.install_missing_servers,
+    ensure_installed = ensure_installed,
+  })
+end
+
+---@param opts RvnLspOptions
+M.config = function(_, opts)
+  require("common.diagnostic").setup()
+  require("common.formatter").setup()
+  setup_language_servers(opts)
+end
+
+return M
---
+++ nvim/config/lua/libs/lsp/null-ls.lua
@@ -0,0 +1,42 @@
+--- @desc: languages linter and formatter
+--- @type LazySpec
+local M = {}
+M[1] = "jose-elias-alvarez/null-ls.nvim"
+M.dependencies = { "williamboman/mason.nvim" }
+M.event = "BufReadPre"
+
+---@param files table
+local with_root_files = function(files)
+  return function(utils)
+    return utils.root_has_file(files)
+  end
+end
+M.opts = function()
+  local nls = require("null-ls")
+  return {
+    sources = {
+      nls.builtins.formatting.shfmt,
+      nls.builtins.formatting.prettierd,
+      nls.builtins.code_actions.eslint_d,
+      nls.builtins.diagnostics.shellcheck,
+      nls.builtins.diagnostics.markdownlint,
+      nls.builtins.diagnostics.eslint_d.with({
+        condition = with_root_files({
+          ".eslint.json",
+          ".eslintrc",
+          ".eslintjs",
+        }),
+      }),
+      nls.builtins.formatting.stylua.with({
+        condition = with_root_files({
+          "stylua.toml",
+          "luarc.json",
+          ".stylua.toml",
+          ".luarc",
+        }),
+      }),
+    },
+  }
+end
+
+return M
