471eebaad54ebfc247be78245bb173746b8ec30c
Radvil <radvil@@access-mobile.com>
2023-06-12 05:53:43 +0800

    activate symbols outline for dev test

--- nvim/config/lua/libs/accessibility/tree/symbols-outline.lua
+++ nvim/config/lua/libs/accessibility/tree/symbols-outline.lua
@@ -1,54 +1,48 @@
 ---@type LazySpec
 local M = {}
 M[1] = "simrat39/symbols-outline.nvim"
-M.enabled = false
--- ---@alias symbolopts table<string, {icon:string,hl:string}>
--- ---@param opts symbolopts
--- local function get_symbols(opts)
---   local icons = require("media.icons").KindIcons
---   for key, _ in pairs(opts) do
---     if opts[key] and icons[key] then
---       opts[key].icon = icons[key]
---     end
---   end
---   return opts
--- end
--- M.opts = function(opts)
---   local highlight_hovered_item = rnv.symbols.highlight_hovered_item or false
---   local relative_width = rnv.symbols.relative_width or false
---   local position = rnv.symbols.position or "right"
---   local wrap = rnv.symbols.wrap or false
---   local auto_close = rnv.symbols.auto_close or false
---   local symbols = get_symbols(opts.symbols or {})
---   local keymaps = vim.tbl_deep_extend("force", opts.keymaps or {}, rnv.symbols.keymaps or {})
---   local width = rnv.symbols.width or 25
---   return {
---     highlight_hovered_item = highlight_hovered_item,
---     relative_width = relative_width,
---     auto_close = auto_close,
---     position = position,
---     symbols = symbols,
---     keymaps = keymaps,
---     width = width,
---     wrap = wrap,
---     show_guides = true,
---     auto_preview = false,
---     show_numbers = false,
---     show_relative_numbers = false,
---     show_symbol_details = false,
---     preview_bg_highlight = "Pmenu",
---     autofold_depth = nil,
---     auto_unfold_hover = true,
---     fold_markers = { "", "" },
---     lsp_blacklist = {},
---     symbol_blacklist = {},
---   }
--- end
+M.enabled = true
+
+M.opts = {
+  highlight_hovered_item = true,
+  relative_width = true,
+  auto_close = false,
+  position = "right",
+  keymaps = {
+    close = { "<esc>", "q" },
+    goto_location = { "<cr>", "o" },
+    focus_location = "l",
+    hover_symbol = "K",
+    toggle_preview = "gh",
+    rename_symbol = "gr",
+    code_actions = "ga",
+    fold = "zc",
+    unfold = "zo",
+    fold_all = "zC",
+    unfold_all = "zO",
+    fold_reset = "zU",
+  },
+  width = 46,
+  wrap = false,
+  show_guides = true,
+  auto_preview = false,
+  show_numbers = false,
+  show_relative_numbers = false,
+  show_symbol_details = false,
+  preview_bg_highlight = "Pmenu",
+  autofold_depth = nil,
+  auto_unfold_hover = true,
+  fold_markers = { "", "" },
+  lsp_blacklist = {},
+  symbol_blacklist = {},
+}
+
 M.keys = {
   {
-    "<C-z>s",
-    "<Cmd>SymbolsOutline<Cr>",
-    desc = "⭐ Toggle symbols window",
+    "<leader>uS",
+    ":SymbolsOutline<cr>",
+    desc = "Toggle » Symbols outline",
   },
 }
+
 return M
