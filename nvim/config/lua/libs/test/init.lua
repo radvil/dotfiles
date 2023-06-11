6c5064fb87a7d4e4920872e2b09aba5ed7e9523d
Radvil <radvil@@access-mobile.com>
2023-06-12 04:33:10 +0800

    testing edgy plugin

--- nvim/config/lua/libs/test/init.lua
+++ nvim/config/lua/libs/test/init.lua
@@ -1,100 +1,74 @@
 ---@type LazySpec[]
+if not rnv.opt.dev then
+  return {}
+end
+
+---@type LazySpec[]
 return {
+  -- prevent neo-tree from opening files in edgy windows
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    optional = true,
+    opts = {
+      open_files_do_not_replace_types = { "terminal", "Trouble", "qf", "edgy" },
+    },
+  },
+
   ---@type LazySpec
   {
     "folke/edgy.nvim",
-    enabled = false,
     event = "VeryLazy",
-    opts = function()
-      return {
-        left = {}, ---@type (Edgy.View.Opts|string)[]
-        bottom = {}, ---@type (Edgy.View.Opts|string)[]
-        right = {}, ---@type (Edgy.View.Opts|string)[]
-        top = {}, ---@type (Edgy.View.Opts|string)[]
-
-        ---@type table<Edgy.Pos, {size:integer, wo?:vim.wo}>
-        options = {
-          left = { size = 30 },
-          bottom = { size = 10 },
-          right = { size = 30 },
-          top = { size = 10 },
-        },
-        -- edgebar animations
-        animate = {
-          enabled = true,
-          fps = 100, -- frames per second
-          cps = 120, -- cells per second
-          on_begin = function()
-            vim.g.minianimate_disable = true
-          end,
-          on_end = function()
-            vim.g.minianimate_disable = false
-          end,
-          -- Spinner for pinned views that are loading.
-          -- if you have noice.nvim installed, you can use any spinner from it, like:
-          -- spinner = require("noice.util.spinners").spinners.circleFull,
-          spinner = {
-            frames = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" },
-            interval = 80,
-          },
-        },
-        -- enable this to exit Neovim when only edgy windows are left
-        exit_when_last = false,
-        -- global window options for edgebar windows
-        ---@type vim.wo
-        wo = {
-          -- Setting to `true`, will add an edgy winbar.
-          -- Setting to `false`, won't set any winbar.
-          -- Setting to a string, will set the winbar to that string.
-          winbar = true,
-          winfixwidth = true,
-          winfixheight = false,
-          winhighlight = "WinBar:EdgyWinBar,Normal:EdgyNormal",
-          spell = false,
-          signcolumn = "no",
-        },
-        -- buffer-local keymaps to be added to edgebar buffers.
-        -- Existing buffer-local keymaps will never be overridden.
-        -- Set to false to disable a builtin.
-        ---@type table<string, fun(win:Edgy.Window)|false>
-        keys = {
-          -- close window
-          ["q"] = function(win)
-            win:close()
-          end,
-          -- hide window
-          ["<c-q>"] = function(win)
-            win:hide()
-          end,
-          -- close sidebar
-          ["Q"] = function(win)
-            win.view.edgebar:close()
-          end,
-          -- next open window
-          ["]w"] = function(win)
-            win:next({ visible = true, focus = true })
-          end,
-          -- previous open window
-          ["[w"] = function(win)
-            win:prev({ visible = true, focus = true })
-          end,
-          -- next loaded window
-          ["]W"] = function(win)
-            win:next({ pinned = false, focus = true })
-          end,
-          -- prev loaded window
-          ["[W"] = function(win)
-            win:prev({ pinned = false, focus = true })
+    init = function()
+      vim.opt.laststatus = 3
+      vim.opt.splitkeep = "screen"
+    end,
+    opts = {
+      bottom = {
+        {
+          ft = "lazyterm",
+          title = "LazyTerm",
+          size = { height = 0.4 },
+          filter = function(buf)
+            return not vim.b[buf].lazyterm_cmd
           end,
         },
-        icons = {
-          closed = " ",
-          open = " ",
+        "Trouble",
+        {
+          ft = "qf",
+          title = "QuickFix"
         },
-        -- enable this on Neovim <= 0.10.0 to properly fold edgebar windows.
-        -- Not needed on a nightly build >= June 5, 2023.
-        fix_win_height = vim.fn.has("nvim-0.10.0") == 0,
-      }
-    end
+        {
+          ft = "help",
+          size = { height = 20 },
+          -- only show help buffers
+          filter = function(buf)
+            return vim.bo[buf].buftype == "help"
+          end,
+        },
+        { ft = "spectre_panel", size = { height = 0.4 } },
+      },
+      left = {
+        -- Neo-tree filesystem always takes half the screen height
+        {
+          title = "WORKDIR",
+          ft = "neo-tree",
+          filter = function(buf)
+            return vim.b[buf].neo_tree_source == "filesystem"
+          end,
+          size = { height = 0.5 },
+        },
+        {
+          title = "BUFFERS",
+          ft = "neo-tree",
+          filter = function(buf)
+            return vim.b[buf].neo_tree_source == "buffers"
+          end,
+          pinned = true,
+          open = "Neotree position=top buffers",
+        },
+        -- any other neo-tree windows
+        "neo-tree",
+      },
+    },
   }
 }
