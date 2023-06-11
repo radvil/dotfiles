e29910b2f63cd033949d110e0352f2c706338049
Radvil <radvil@@access-mobile.com>
2023-06-11 09:13:52 +0800

    move lsp modules

--- nvim/config/lua/usr/lsp/init.lua
+++
@@ -1,9 +0,0 @@
-return {
-  require("usr.lsp.mason"),
-  require("usr.lsp.null-ls"),
-  require("usr.lsp.lspconfig"),
-  require("usr.lsp.lang.typescript"),
-  require("usr.lsp.lang.angular"),
-  require("usr.lsp.lang.emmet"),
-  require("usr.lsp.lang.go"),
-}
