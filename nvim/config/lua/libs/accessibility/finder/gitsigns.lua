541279b6906dceeafbefbe49e00dd80c3593d29e
Radvil <radvil@@access-mobile.com>
2023-06-11 11:21:28 +0800

    adjust keymaps + desc

--- nvim/config/lua/libs/accessibility/finder/gitsigns.lua
+++ nvim/config/lua/libs/accessibility/finder/gitsigns.lua
@@ -3,27 +3,30 @@ local M = {}
 M[1] = "lewis6991/gitsigns.nvim"
 M.enabled = true
 M.event = "BufReadPre"
--- TODO: change ugly icons
-local signs = {
-  add = { text = "▎" },
-  change = { text = "▎" },
-  delete = { text = "契" },
-  topdelete = { text = "契" },
-  changedelete = { text = "▎" },
-  untracked = { text = "▎" },
-}
+
+-- -- TODO: change ugly icons
+-- local signs = {
+--   add = { text = "▎" },
+--   change = { text = "▎" },
+--   delete = { text = "契" },
+--   topdelete = { text = "契" },
+--   changedelete = { text = "▎" },
+--   untracked = { text = "▎" },
+-- }
+
 M.opts = {
-  signs = signs,
+  -- signs = signs,
   on_attach = function(buffer)
     local function gs()
       return package.loaded.gitsigns
     end
     vim.keymap.set("n", "<c-z>g", function()
       gs().toggle_current_line_blame()
-    end, { buffer = buffer, desc = "git sign » toggle blame line" })
+    end, { buffer = buffer, desc = "Toggle » git blame line" })
     vim.keymap.set("n", "<Leader>gd", function()
       gs().diffthis()
-    end, { buffer = buffer, desc = "git sign » diff buffer" })
+    end, { buffer = buffer, desc = "Git sign » diff current buffer" })
   end,
 }
+
 return M
--- nvim/config/lua/libs/ui/notify.lua
+++ nvim/config/lua/libs/ui/notify.lua
@@ -5,18 +5,17 @@ M.enabled = true
 M.event = "BufReadPost"
 
 M.opts = function(_, opts)
-  if not rnv.opt.dev then
-    opts.timeout = 1000
-  else
+  opts.timeout = 1000
+  if rnv.opt.transbg then
+    opts.background_colour = "#000000"
+  end
+  if rnv.opt.dev then
     opts.timeout = 10000
     opts.max_height = function()
       return math.floor(vim.o.lines * 0.75)
     end
     opts.max_width = function()
       return math.floor(vim.o.columns * 0.36)
-    end
-    if rnv.opt.transbg then
-      opts.background_colour = "#000000"
     end
   end
   return opts
@@ -28,7 +27,7 @@ end
 
 M.keys = {
   {
-    "<c-z>n",
+    "<c-z>n", -- TODO: Toggle notification
     function()
       require("notify").dismiss({
         silent = true,
