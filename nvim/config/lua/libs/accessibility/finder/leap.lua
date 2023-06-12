4ed6dc3f4bd8ae271368d06dc4400b17fbe248d4
Radvil <radvil@@access-mobile.com>
2023-06-12 23:08:02 +0800

    add leap.nvim + flit.nvim
    + register custom keymap
    + replace default <cr> for flit.nvim

--- nvim/config/lua/libs/accessibility/finder/leap.lua
+++ nvim/config/lua/libs/accessibility/finder/leap.lua
@@ -1,52 +1,64 @@
----@desc: motion jumper
----@type LazySpec
-local M = {}
+if false then
+  return {}
+end
 
-M[1] = "ggandor/leap.nvim"
-
-M.enabled = false
-
--- M.event = "VeryLazy"
-
--- M.dependencies = {
---   "ggandor/flit.nvim",
---   opts = {
---     labeled_modes = "nv",
---     keys = {
---       f = "f",
---       F = "F",
---       t = "t",
---       T = "T",
---     },
---   },
--- }
-
-M.opts = {
-  highlight_unlabeled_phase_one_targets = false,
-  safe_labels = {},
-  labels = {},
-  case_sensitive = false,
-  special_keys = {
-    next_target = { "<Enter>", "<Tab>" },
-    prev_target = { "<S-Enter>", "," },
-    next_group = "<Space>",
-    prev_group = "<Tab>",
-  },
-}
-
-local leap_mode = { "n", "x", "v", "o" }
-
-M.keys = {
+---@type LazySpec[]
+return {
   {
-    "m",
-    "<Plug>(leap-forward-to)",
-    mode = leap_mode,
+    "ggandor/leap.nvim",
+    enabled = true,
+    keys = {
+      {
+        "m",
+        function()
+          require("leap").leap({
+            target_windows = {
+              vim.fn.win_getid()
+            }
+          })
+        end,
+        desc = "Leap » Accross current window",
+        mode = { "n", "x", "o" },
+      },
+      {
+        "<a-w>",
+        function()
+          require('leap').leap {
+            target_windows = vim.tbl_filter(
+              function(win) return vim.api.nvim_win_get_config(win).focusable end,
+              vim.api.nvim_tabpage_list_wins(0)
+            )
+          }
+        end,
+        desc = "Leap » Accross all windows",
+        mode = "n",
+      },
+    },
+    config = function()
+      local leap = require("leap")
+      leap.opts.case_sensitive = false
+      -- NOTE: IDK why I need these
+      leap.opts.special_keys = {
+        repeat_search = '<enter>',
+        next_phase_one_target = '<enter>',
+        next_target = { '<enter>', ';' },
+        prev_target = { '<tab>', ',' },
+        multi_revert = '<backspace>',
+        multi_accept = '<enter>',
+        next_group = '<space>',
+        prev_group = '<tab>',
+      }
+    end
   },
   {
-    "<C-m>",
-    "<Plug>(leap-backward-to)",
-    mode = leap_mode,
-  },
+    "ggandor/flit.nvim",
+    dependencies = "ggandor/leap.nvim",
+    config = function()
+      local opts = {
+        keys = { f = ']f', F = '[f', t = ']t', T = '[t' },
+        labeled_modes = "nxo",
+      }
+      require("flit").setup(opts)
+    end
+  }
 }
-
-return M
