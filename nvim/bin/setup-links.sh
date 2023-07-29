b9b51b8325ebf3a47e5d59ec398d999b1741f4ca
Radvil <radvil@@access-mobile.com>
2023-07-29 22:21:01 +0800

    Update nvim config setup links

--- nvim/bin/setup-links.sh
+++ nvim/bin/setup-links.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
-ln -sf "$DOTFILES/nvim/configs/default" "$HOME/.config/nvim"
+ln -sf "$DOTFILES/nvim/configs/lazyvim" "$HOME/.config/nvim"
 ln -sf "$DOTFILES/nvim/configs/radvil" "$HOME/.config/radvil"
+ln -sf "$DOTFILES/nvim/configs/nvmini" "$HOME/.config/nvmini"
 ln -sf "$DOTFILES/nvim/configs/lazyvim" "$HOME/.config/lazyvim"
 ln -sf "$DOTFILES/nvim/configs/nvchad" "$HOME/.config/nvchad"
