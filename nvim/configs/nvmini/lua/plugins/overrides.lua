4e7fb2bc810c8ac5aa944a1354808519a384a078
Radvil <radvil@@access-mobile.com>
2024-01-10 01:00:44 +0800

    override builting notify opts

--- nvim/configs/nvmini/lua/plugins/overrides.lua
+++ nvim/configs/nvmini/lua/plugins/overrides.lua
@@ -39,6 +39,8 @@ return {
     "rcarriga/nvim-notify",
     optional = true,
     opts = function(_, opts)
+      opts.render = "wrapped-compact"
+      opts.stages = "fade"
       if type(opts.banned_messages) == "table" then
         vim.list_extend(opts.banned_messages, { "No information available" })
       end
