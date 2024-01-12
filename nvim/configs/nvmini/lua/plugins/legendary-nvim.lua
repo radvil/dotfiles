91ce364d0159de69fb639ca51b367dec1975ad35
Radvil <radvil@@access-mobile.com>
2024-01-13 03:11:49 +0800

    add gitsigns keymap on legendary index

--- nvim/configs/nvmini/lua/plugins/legendary-nvim.lua
+++ nvim/configs/nvmini/lua/plugins/legendary-nvim.lua
@@ -12,6 +12,7 @@ return {
     select_prompt = " Quick command/keymaps ",
     include_legendary_cmds = false,
     include_builtin = true,
+    keymaps = {},
     extensions = {
       diffview = true,
       smart_splits = false,
@@ -22,10 +23,65 @@ return {
     },
   },
   config = function(_, opts)
-    if require("neoverse.utils").lazy_has("which-key.nvim") then
+    local Utils = require("neoverse.utils")
+    if Utils.lazy_has("which-key.nvim") then
       opts.extensions.which_key = {
         auto_register = true,
       }
+    end
+    if Utils.lazy_has("gitsigns.nvim") then
+      vim.list_extend(opts.keymaps, {
+        {
+          "<leader>gr",
+          function()
+            require("gitsigns").refresh()
+            vim.schedule(function()
+              require("neoverse.utils").info("refreshed", { title = "gitsigns" })
+            end)
+          end,
+          description = "gitsigns » refresh view",
+        },
+        {
+          "<leader>gun",
+          function()
+            require("gitsigns").toggle_numhl()
+            vim.schedule(function()
+              require("neoverse.utils").info("numbers toggled", { title = "gitsigns" })
+            end)
+          end,
+          description = "gitsigns » toggle numbers",
+        },
+        {
+          "<leader>guu",
+          function()
+            require("gitsigns").toggle_signs()
+            vim.schedule(function()
+              require("neoverse.utils").info("signs toggled", { title = "gitsigns" })
+            end)
+          end,
+          description = "gitsigns » toggle signs",
+        },
+        {
+          "<leader>gud",
+          function()
+            require("gitsigns").toggle_word_diff()
+            vim.schedule(function()
+              require("neoverse.utils").info("word diff toggled", { title = "gitsigns" })
+            end)
+          end,
+          description = "gitsigns » toggle word diff",
+        },
+        {
+          "<leader>gub",
+          function()
+            require("gitsigns").toggle_current_line_blame()
+            vim.schedule(function()
+              require("neoverse.utils").info("line blame toggled", { title = "gitsigns" })
+            end)
+          end,
+          description = "gitsigns » toggle current line blame",
+        },
+      })
     end
     require("legendary").setup(opts)
   end,
