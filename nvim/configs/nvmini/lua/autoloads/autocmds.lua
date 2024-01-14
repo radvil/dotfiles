5fb8cd40811a3e7d9a6f76c8b2de8e227f8b5be9
Radvil <radvil@@access-mobile.com>
2024-01-15 03:55:36 +0800

    fix(autocmd): toggle cursorline properly

--- nvim/configs/nvmini/lua/autoloads/autocmds.lua
+++ nvim/configs/nvmini/lua/autoloads/autocmds.lua
@@ -1,5 +1,4 @@
--- local Utils = require("neoverse.utils")
--- local palette = require("neoverse.config").palette
+local Utils = require("neoverse.utils")
 
 -- vim.cmd([[:amenu 10.90 mousemenu.Select\ all <cmd>normal ggVG<CR>]])
 -- vim.cmd([[:amenu 10.100 mousemenu.Goto\ definitions <cmd>Telescope lsp_definitions<CR>]])
@@ -9,18 +8,17 @@
 if vim.opt.cursorline then
   vim.opt.guicursor = ""
 
-  -- TODO: do this better
-  -- vim.api.nvim_create_autocmd("InsertEnter", {
-  --   group = Utils.create_augroup("cursor_insert"),
-  --   callback = function()
-  --     vim.api.nvim_set_hl(0, "CursorLine", { bg = "NONE" })
-  --   end,
-  -- })
-  --
-  -- vim.api.nvim_create_autocmd("InsertLeave", {
-  --   group = Utils.create_augroup("cursor_normal"),
-  --   callback = function()
-  --     vim.api.nvim_set_hl(0, "CursorLine", { bg = palette.dark2 })
-  --   end,
-  -- })
+  vim.api.nvim_create_autocmd("InsertEnter", {
+    group = Utils.create_augroup("cursor_insert"),
+    callback = function()
+      vim.wo.cursorline = false
+    end,
+  })
+
+  vim.api.nvim_create_autocmd("InsertLeave", {
+    group = Utils.create_augroup("cursor_normal"),
+    callback = function()
+      vim.wo.cursorline = true
+    end,
+  })
 end
