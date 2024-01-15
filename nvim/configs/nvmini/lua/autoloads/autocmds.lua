97d112cbb4273a3ea330b259cc07d059013f0290
Radvil <radvil@@access-mobile.com>
2024-01-15 10:43:31 +0800

    only run cursorline toggle on the actual file buffer

--- nvim/configs/nvmini/lua/autoloads/autocmds.lua
+++ nvim/configs/nvmini/lua/autoloads/autocmds.lua
@@ -11,14 +11,18 @@ if vim.opt.cursorline then
   vim.api.nvim_create_autocmd("InsertEnter", {
     group = Utils.create_augroup("cursor_insert"),
     callback = function()
-      vim.wo.cursorline = false
+      if vim.bo.buftype == "" then
+        vim.wo.cursorline = false
+      end
     end,
   })
 
   vim.api.nvim_create_autocmd("InsertLeave", {
     group = Utils.create_augroup("cursor_normal"),
     callback = function()
-      vim.wo.cursorline = true
+      if vim.bo.buftype == "" then
+        vim.wo.cursorline = true
+      end
     end,
   })
 end
