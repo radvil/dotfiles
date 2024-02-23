ea5f0f8e429234c5c2da52bb8ad43978d6400676
Radvil <radvil@@access-mobile.com>
2024-02-23 22:03:29 +0800

    feat(nvmini): toggle cursorline autocmd

--- nvim/configs/nvmini/lua/autoloads/autocmds.lua
+++ nvim/configs/nvmini/lua/autoloads/autocmds.lua
@@ -1,19 +1,23 @@
-local Utils = require("neoverse.utils")
-
-vim.api.nvim_create_autocmd("InsertEnter", {
-  group = Utils.create_augroup("cursor_insert"),
-  callback = function()
-    if vim.opt.cursorline and vim.bo.buftype == "" then
-      vim.wo.cursorline = false
-    end
-  end,
-})
-
-vim.api.nvim_create_autocmd("InsertLeave", {
-  group = Utils.create_augroup("cursor_normal"),
-  callback = function()
-    if vim.opt.cursorline and vim.bo.buftype == "" then
-      vim.wo.cursorline = true
-    end
-  end,
-})
+-- local Utils = require("neoverse.utils")
+--
+-- vim.api.nvim_create_autocmd({
+--   "InsertEnter", --[[ "WinLeave"  ]]
+-- }, {
+--   group = Utils.create_augroup("cursor_insert"),
+--   callback = function()
+--     if vim.opt.cursorline and vim.bo.buftype == "" then
+--       vim.wo.cursorline = false
+--     end
+--   end,
+-- })
+--
+-- vim.api.nvim_create_autocmd({
+--   "InsertLeave", --[[ "WinEnter"  ]]
+-- }, {
+--   group = Utils.create_augroup("cursor_normal"),
+--   callback = function()
+--     if vim.opt.cursorline and vim.bo.buftype == "" then
+--       vim.wo.cursorline = true
+--     end
+--   end,
+-- })
