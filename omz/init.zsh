a24ccf5b20749b02cab3d5b7d9644e17e7a41b29
Radvil <radvil.linux@gmail.com>
2024-03-12 02:33:27 +0800

    feat(omz): preview vi-mode

--- omz/init.zsh
+++ omz/init.zsh
@@ -1,5 +1,5 @@
 #!/usr/bin/env zsh
 
-# plugins=(npm vi-mode node aws colored-man-pages)
-plugins=(npm node aws colored-man-pages)
+plugins=(npm vi-mode node aws colored-man-pages)
+# plugins=(npm node aws colored-man-pages)
 source_file "$HOME/.oh-my-zsh/oh-my-zsh.sh" --silent
