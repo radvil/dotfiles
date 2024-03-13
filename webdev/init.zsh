db44c101fd02cec37eb84e4682496764a2d8e220
Radvil <radvil.linux@gmail.com>
2024-03-14 03:37:33 +0800

    perf(zshrv): Do not source nvm script by default

--- webdev/init.zsh
+++ webdev/init.zsh
@@ -6,7 +6,19 @@ if [ -z "${BUN_INSTALL}" ]; then
   export BUN_INSTALL
 fi
 
-if [ -z "${NVM_DIR}" ]; then
-  [ -s "$HOME/.nvm/nvm.sh" ] && source "$HOME/.nvm/nvm.sh"
-  [ -s "$HOME/.nvm/bash_completion" ] && source "$HOME/.nvm/bash_completion"
+
+if ! command -v nvm &> /dev/null; then
+  # HACK: prevent sourcing every time a new instance of zsh spawned
+  # becaues it is too much slow
+  function load_nvm() {
+    [ -s "$HOME/.nvm/nvm.sh" ] && source "$HOME/.nvm/nvm.sh"
+    [ -s "$HOME/.nvm/bash_completion" ] && source "$HOME/.nvm/bash_completion"
+    nvm ls
+    printf "\033[2K[\033[00;32m✔ OKAY\033[0m] nvm loaded\n"
+  }
 fi
+
+# if [ -z "${NVM_DIR}" ]; then
+#   [ -s "$HOME/.nvm/nvm.sh" ] && source "$HOME/.nvm/nvm.sh"
+#   [ -s "$HOME/.nvm/bash_completion" ] && source "$HOME/.nvm/bash_completion"
+# fi
