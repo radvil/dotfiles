d74788676ef97c2ff377987b6a89701e10c3d5c6
Radvil <radvil.linux@gmail.com>
2024-03-16 20:01:40 +0800

    feat(autocmd): Remove comments

--- nvim/configs/nvmini/lua/autoloads/autocmds.lua
+++ nvim/configs/nvmini/lua/autoloads/autocmds.lua
@@ -1,27 +1,5 @@
 local Utils = require("neoverse.utils")
 
--- vim.api.nvim_create_autocmd({
---   "InsertEnter", --[[ "WinLeave"  ]]
--- }, {
---   group = Utils.create_augroup("cursor_insert"),
---   callback = function()
---     if vim.opt.cursorline and vim.bo.buftype == "" then
---       vim.wo.cursorline = false
---     end
---   end,
--- })
---
--- vim.api.nvim_create_autocmd({
---   "InsertLeave", --[[ "WinEnter"  ]]
--- }, {
---   group = Utils.create_augroup("cursor_normal"),
---   callback = function()
---     if vim.opt.cursorline and vim.bo.buftype == "" then
---       vim.wo.cursorline = true
---     end
---   end,
--- })
-
 vim.api.nvim_create_autocmd({ "FileType" }, {
   group = Utils.create_augroup("leaving_cwd"),
   callback = function()
