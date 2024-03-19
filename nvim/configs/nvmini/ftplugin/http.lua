0c718c1d113add8e7120e59888af5fa60b451712
Radvil <radvil.linux@gmail.com>
2024-03-20 00:13:02 +0800

    feat(rest-nvim): Lock the old implementation (v1)

--- nvim/configs/nvmini/ftplugin/http.lua
+++ nvim/configs/nvmini/ftplugin/http.lua
@@ -1,6 +1,5 @@
 local Utils = require("neoverse.utils")
 
--- @commit 91badd46c60df6bd9800c809056af2d80d33da4c
 Utils.map("n", "<cr>", "<Plug>RestNvim", {
   desc = "rest.nvim » run the request under the cursor",
   buffer = true,
@@ -10,13 +9,3 @@ Utils.map("n", "<c-r>", "<Plug>RestNvimLast", {
   desc = "rest.nvim » run last request",
   buffer = true,
 })
-
--- Utils.map("n", "<cr>", "<cmd>Rest run<cr>", {
---   desc = "rest.nvim » run the request under the cursor",
---   buffer = true,
--- })
---
--- Utils.map("n", "<c-r>", "<cmd>Rest run last<cr>", {
---   desc = "rest.nvim » run last request",
---   buffer = true,
--- })
