b58be1c5092dbda283a0953bea7b2e2aca60148b
Radvil <radvil.linux@gmail.com>
2024-03-20 19:53:03 +0800

    feat(autocmd): Toggle leaveing cwd notification

--- nvim/configs/nvmini/lua/autoloads/autocmds.lua
+++ nvim/configs/nvmini/lua/autoloads/autocmds.lua
@@ -1,12 +1,12 @@
-local Utils = require("neoverse.utils")
-
-vim.api.nvim_create_autocmd({ "FileType" }, {
-  group = Utils.create_augroup("leaving_cwd"),
-  callback = function()
-    if vim.bo.buftype == "" then
-      if Utils.root() ~= vim.loop.cwd() then
-        Utils.warn("Leaving [cwd] to " .. Utils.root(), { title = "Warning" })
-      end
-    end
-  end,
-})
+-- local Utils = require("neoverse.utils")
+--
+-- vim.api.nvim_create_autocmd({ "FileType" }, {
+--   group = Utils.create_augroup("leaving_cwd"),
+--   callback = function()
+--     if vim.bo.buftype == "" then
+--       if Utils.root() ~= vim.loop.cwd() then
+--         Utils.warn("Leaving [cwd] to " .. Utils.root(), { title = "Warning" })
+--       end
+--     end
+--   end,
+-- })
