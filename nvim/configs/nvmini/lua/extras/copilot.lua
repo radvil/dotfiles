5e2c3e3488bd4070271136783986b45e148f3baa
Radvil <radvil.linux@gmail.com>
2024-03-20 19:53:21 +0800

    feat(extras): Add copilot.lua

---
+++ nvim/configs/nvmini/lua/extras/copilot.lua
@@ -0,0 +1,75 @@
+local activated = false
+
+return {
+  {
+    "lualine.nvim",
+    optional = true,
+    event = "VeryLazy",
+    opts = function(_, opts)
+      table.insert(opts.sections.lualine_y, 2, require("neoverse.utils").lualine.cmp_source("copilot"))
+    end,
+  },
+
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    keys = {
+      {
+        "<leader>ug",
+        function()
+          if activated then
+            vim.cmd("Copilot disable")
+            require("neoverse.utils").warn("server disabled", { title = "Copilot" })
+          else
+            vim.cmd("Copilot enable")
+            require("neoverse.utils").info("server enabled", { title = "Copilot" })
+          end
+          activated = not activated
+        end,
+        desc = "Toggle » Github Copilot",
+      },
+    },
+    opts = {
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+      filetypes = {
+        markdown = true,
+        help = true,
+      },
+    },
+    config = function(_, opts)
+      require("copilot").setup(opts)
+      if not activated then
+        vim.cmd("Copilot disable")
+      end
+    end,
+  },
+
+  {
+    "zbirenbaum/copilot-cmp",
+    dependencies = "copilot.lua",
+    config = function()
+      local copilot_cmp = require("copilot_cmp")
+      copilot_cmp.setup({ fix_pairs = true })
+      -- attach cmp source whenever copilot attaches
+      -- fixes lazy-loading issues with the copilot cmp source
+      require("neoverse.utils").lsp.on_attach(function(client)
+        if client.name == "copilot" and activated then
+          copilot_cmp._on_insert_enter({})
+        end
+      end)
+    end,
+  },
+
+  {
+    "nvim-cmp",
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      table.insert(opts.sources, 1, {
+        name = "copilot",
+        group_index = 1,
+        priority = 100,
+      })
+    end,
+  },
+}
