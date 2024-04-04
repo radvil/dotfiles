2d4a4a542934cc98fe51d5e7097eb6f666a96ef8
Radvil <radvil.linux@gmail.com>
2024-04-04 22:40:43 +0800

    feat(sticky-buf): custom excludes optons

--- nvim/configs/nvmini/lua/extras/misc/sticky-buff.lua
+++ nvim/configs/nvmini/lua/extras/misc/sticky-buff.lua
@@ -1,5 +1,19 @@
 return {
   "stevearc/stickybuf.nvim",
   event = "BufAdd",
-  opts = {},
+  opts = {
+    neo_excludes = {
+      "Outline",
+    },
+  },
+  config = function(_, opts)
+    require("stickybuf").setup({
+      get_auto_pin = function(bufnr)
+        local filetype = vim.bo[bufnr].filetype
+        if vim.tbl_contains(opts.neo_excludes, filetype) then
+          return "filetype"
+        end
+      end,
+    })
+  end,
 }
