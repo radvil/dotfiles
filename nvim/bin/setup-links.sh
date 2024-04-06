beb2845a7f076734fe88c3b6e38bc094b7b9968f
Radvil <radvil.linux@gmail.com>
2024-04-06 23:35:54 +0800

    feat(nvim-install): POSIX compatibility

--- nvim/bin/setup-links.sh
+++ nvim/bin/setup-links.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
 __install__() {
-	local source_dir="$DOTFILES/nvim/configs"
-	local target_dir="$HOME/.config"
+	source_dir="$DOTFILES/nvim/configs"
+	target_dir="$HOME/.config"
 	ln -sf "$source_dir/nvmini" "$target_dir/nvim"
 	ln -sf "$source_dir/nvmini" "$target_dir/nvmini"
 	ln -sf "$source_dir/lazyvim" "$target_dir/lazyvim"
