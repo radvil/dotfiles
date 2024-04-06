18d590085244acf8c47cba74f286ea5cb4bd7587
Radvil <radvil.linux@gmail.com>
2024-04-06 23:33:16 +0800

    remove unused test functions

--- nvim/configs/nvmini/lua/test-functions.lua
+++ nvim/configs/nvmini/lua/test-functions.lua
@@ -18,37 +18,6 @@ M.test_input = function()
   end)
 end
 
-M.image_nvim = function()
-  package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?/init.lua;"
-  package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?.lua;"
-
-  vim.opt.number = true
-  vim.opt.conceallevel = 2
-  vim.opt.winbar = "image.nvim demo"
-  vim.opt.signcolumn = "yes:2"
-
-  local content = [[
-# Hello World
-
-![This is a remote image](https://gist.ro/s/remote.png)
-]]
-
-  vim.schedule(function()
-    local buf = vim.api.nvim_create_buf(false, true)
-    vim.api.nvim_buf_set_lines(buf, 0, -1, true, vim.split(content, "\n"))
-    vim.api.nvim_buf_set_option(buf, "filetype", "markdown")
-    vim.api.nvim_set_current_buf(buf)
-    vim.cmd("split")
-  end)
-end
-
-M.find_filepath = function()
-  local fpath = vim.fn.fnamemodify(vim.fn.expand("%"), ":~:.:h")
-  -- vim.notify(fpath) -- ~/.dotfiles/nvim/configs/nvmini/lua
-  local basename = vim.fs.basename(fpath)
-  vim.notify(basename) -- ~/.dotfiles/nvim/configs/nvmini/lua
-end
-
 M.debug_plugins = function()
   local nsn = vim.api.nvim_get_namespaces()
   local counts = {}
@@ -73,6 +42,7 @@ end
 
 M.main = function()
   -- vim.print(vim.uv.os_uname());
+  -- vim.print(vim.fn.stdpath("state"))
   M.debug_plugins()
 end
 
