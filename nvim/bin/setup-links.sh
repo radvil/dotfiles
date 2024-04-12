a523b2e7594c178bc00a60e86d4e8112d28780c3
Radvil <radvil.linux@gmail.com>
2024-04-13 00:57:14 +0800

    feat(nvim-configs): remove old `lazyvim`. use `default` variant

--- nvim/bin/setup-links.sh
+++ nvim/bin/setup-links.sh
@@ -3,9 +3,8 @@
 __install__() {
 	source_dir="$DOTFILES/nvim/configs"
 	target_dir="$HOME/.config"
-	ln -sf "$source_dir/nvmini" "$target_dir/nvim"
+	ln -sf "$source_dir/default" "$target_dir/nvim"
 	ln -sf "$source_dir/nvmini" "$target_dir/nvmini"
-	ln -sf "$source_dir/lazyvim" "$target_dir/lazyvim"
 }
 
 __install__
