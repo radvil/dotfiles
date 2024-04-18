21bec6926cfd32a90f2a6e85fab512b925f09c53
Radvil <radvil.linux@gmail.com>
2024-04-18 19:46:38 +0800

    feat(fzf): set custom colorscheme

--- fzf/init.zsh
+++ fzf/init.zsh
@@ -3,6 +3,10 @@
 # provide cutom values for fzf
 export FZF_TMUX=1
 export FZF_TMUX_OPTS="-p -h 80% -w 60% --layout=reverse --border"
+export FZF_DEFAULT_OPTS=" \
+--color=bg+:#30303b,bg:#202027,spinner:#f4dbd6,hl:#ff657a \
+--color=fg:#ACB6D2,header:#ff657a,info:#b18eab,pointer:#f4dbd6 \
+--color=marker:#f4dbd6,fg+:#ACB6D2,prompt:#b18eab,hl+:#ff657a"
 
 # custom file finder
 function _fzf_compgen_path() {
