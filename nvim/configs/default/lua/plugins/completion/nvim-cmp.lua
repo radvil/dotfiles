36f68b9fea79f736d9d72dac1e464e2706ec8dbe
Radvil <radvil.linux@gmail.com>
2024-05-25 06:57:16 +0800

    preview(keymap): use builting lazyvim keymap

--- nvim/configs/default/lua/plugins/completion/nvim-cmp.lua
+++ nvim/configs/default/lua/plugins/completion/nvim-cmp.lua
@@ -2,9 +2,9 @@ return {
   "hrsh7th/nvim-cmp",
   optional = true,
   opts = function(_, opts)
-    opts.mapping["<C-CR>"] = nil
-    opts.mapping["<S-CR>"] = nil
-    opts.mapping["<CR>"] = nil
+    -- opts.mapping["<C-CR>"] = nil
+    -- opts.mapping["<S-CR>"] = nil
+    -- opts.mapping["<CR>"] = nil
     if vim.g.neo_transparent then
       local cmp = require("cmp")
       opts.window = {
