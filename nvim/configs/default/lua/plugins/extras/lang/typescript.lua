6e03497a6cf43003d44443ae0d3368bac6e48b7f
Radvil <radvil.linux@gmail.com>
2024-05-31 06:22:00 +0800

    feat(nvim-default): Use `vtsls`

--- nvim/configs/default/lua/plugins/extras/lang/typescript.lua
+++ nvim/configs/default/lua/plugins/extras/lang/typescript.lua
@@ -1,20 +1,34 @@
----@diagnostic disable: missing-fields, assign-type-mismatch
-
-local inlay_hints_settings = {
-  includeInlayEnumMemberValueHints = true,
-  includeInlayFunctionLikeReturnTypeHints = true,
-  includeInlayFunctionParameterTypeHints = true,
-  includeInlayParameterNameHints = "literal",
-  includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-  includeInlayPropertyDeclarationTypeHints = true,
-  includeInlayVariableTypeHints = false,
-  includeInlayVariableTypeHintsWhenTypeMatchesName = false,
-}
+---@diagnostic disable: missing-fields
 
 ---@type LazySpec[]
 return {
+  -- desc = "Typescript LSP configuration",
+  recommended = function()
+    return Lonard.extras.wants({
+      ft = {
+        "javascript",
+        "javascriptreact",
+        "javascript.jsx",
+        "typescript",
+        "typescriptreact",
+        "typescript.tsx",
+      },
+      root = { "tsconfig.json", "package.json", "jsconfig.json" },
+    })
+  end,
+
+  {
+    "yioneko/nvim-vtsls",
+    lazy = true,
+    opts = {},
+    config = function(_, opts)
+      require("vtsls").config(opts)
+    end,
+  },
+
   {
     "nvim-treesitter",
+    optional = true,
     opts = function(_, opts)
       if type(opts.ensure_installed) == "table" then
         vim.list_extend(opts.ensure_installed, {
@@ -25,70 +39,102 @@ return {
       end
     end,
   },
+
   {
     "nvim-lspconfig",
     opts = {
       servers = {
-        ---@type lspconfig.options.tsserver
-        tsserver = {
-          -- mason = false,
-          -- cmd = {
-          --   os.getenv("HOME") .. "/.bun/bin/typescript-language-server",
-          --   "--stdio",
-          -- },
+        vtsls = {
+          settings = {
+            complete_function_calls = true,
+            vtsls = {
+              enableMoveToFileCodeAction = true,
+              experimental = {
+                completion = {
+                  enableServerSideFuzzyMatch = true,
+                },
+              },
+            },
+            typescript = {
+              updateImportsOnFileMove = { enabled = "always" },
+              suggest = {
+                completeFunctionCalls = true,
+              },
+              inlayHints = {
+                enumMemberValues = { enabled = true },
+                functionLikeReturnTypes = { enabled = true },
+                parameterNames = { enabled = "literals" },
+                parameterTypes = { enabled = true },
+                propertyDeclarationTypes = { enabled = true },
+                variableTypes = { enabled = false },
+              },
+            },
+          },
           keys = {
+            {
+              "gs",
+              function()
+                require("vtsls").commands.goto_source_definition(0)
+              end,
+              desc = "Goto Source Definition",
+            },
+            {
+              "gr",
+              function()
+                require("vtsls").commands.file_references(0)
+              end,
+              desc = "File References",
+            },
             {
               "<leader>co",
               function()
-                vim.lsp.buf.code_action({
-                  apply = true,
-                  context = {
-                    only = { "source.organizeImports.ts" },
-                    diagnostics = {},
-                  },
-                })
+                require("vtsls").commands.organize_imports(0)
               end,
-              desc = "[o]rganize Imports",
-            },
-            {
-              "<leader>cc",
-              function()
-                vim.lsp.buf.code_action({
-                  apply = true,
-                  context = {
-                    only = { "source.removeUnused.ts" },
-                    diagnostics = {},
-                  },
-                })
-              end,
-              desc = "[c]lear Unused Imports",
+              desc = "Organize Imports",
             },
             {
               "<leader>cm",
               function()
-                vim.lsp.buf.code_action({
-                  apply = true,
-                  context = {
-                    only = { "source.addMissingImports.ts" },
-                    diagnostics = {},
-                  },
-                })
+                require("vtsls").commands.add_missing_imports(0)
               end,
-              desc = "Add [m]issing Imports",
+              desc = "Add missing imports",
             },
-          },
-          settings = {
-            typescript = {
-              inlayHints = inlay_hints_settings,
+            {
+              "<leader>cc",
+              function()
+                require("vtsls").commands.remove_unused_imports(0)
+              end,
+              desc = "Remove unused imports",
             },
-            javascript = {
-              inlayHints = inlay_hints_settings,
+            {
+              "<leader>cH",
+              function()
+                require("vtsls").commands.fix_all(0)
+              end,
+              desc = "Fix all diagnostics",
             },
-            completions = {
-              completeFunctionCalls = true,
+            {
+              "<leader>cV",
+              function()
+                require("vtsls").commands.select_ts_version(0)
+              end,
+              desc = "Select TS workspace version",
             },
           },
         },
+      },
+      standalone_setups = {
+        vtsls = function(_, opts)
+          -- copy typescript settings to javascript
+          opts.settings.javascript =
+            vim.tbl_deep_extend("force", {}, opts.settings.typescript, opts.settings.javascript or {})
+          local plugins = vim.tbl_get(opts.settings, "vtsls", "tsserver", "globalPlugins")
+          -- allow plugins to have a key for proper merging
+          -- remove the key here
+          if plugins then
+            opts.settings.vtsls.tsserver.globalPlugins = vim.tbl_values(plugins)
+          end
+        end,
       },
     },
   },
