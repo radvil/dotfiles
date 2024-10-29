36c14b9b7044cad7ab964ba55495fd33a788cd9c
Radvil <radvil.linux@gmail.com>
2024-10-29 19:01:44 +0800

    feat(fedora): updates before reinstall

--- nvim/configs/default/lua/plugins/ui/noice.lua
+++ nvim/configs/default/lua/plugins/ui/noice.lua
@@ -11,7 +11,7 @@ return {
 
   {
     "folke/noice.nvim",
-    enabled = true,
+    enabled = false,
     keys = function ()
       return {
         { "<c-d>", function() if not require("noice.lsp").scroll(4) then return "<c-d>" end end, expr = true, desc = "Noice » Scroll forward", mode = {"i", "n", "s"} },
