5400b319b8166ff8af81dcc8dbcc2cb41ebc606f
Radvil <radvil.linux@gmail.com>
2025-08-02 13:04:35 +0800

    feat(nvim): remove unused `split/join` keymaps

--- neovim/src/lazyvim/lua/plugins/extras/editor/mini-splitjoin.lua
+++ neovim/src/lazyvim/lua/plugins/extras/editor/mini-splitjoin.lua
@@ -8,20 +8,6 @@ return {
       end,
       desc = "split/[j]oin",
     },
-    {
-      "gS",
-      function()
-        require("mini.splitjoin").split()
-      end,
-      desc = "[S]plit arguments",
-    },
-    {
-      "gJ",
-      function()
-        require("mini.splitjoin").join()
-      end,
-      desc = "[J]oin arguments",
-    },
   },
   opts = {
     mappings = {
