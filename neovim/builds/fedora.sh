ca910fc63cca854ca363d1531bb9c6c0b7fc82b0
Radvil <radvil.linux@gmail.com>
2025-08-09 23:14:19 +0800

    fix(neovim): Remove goddamn coma

--- neovim/builds/fedora.sh
+++ neovim/builds/fedora.sh
@@ -3,7 +3,7 @@
 function install_neovim() {
   sudo dnf copr enable foopsss/shell-color-scripts -y &&
     sudo dnf copr enable agriffis/neovim-nightly -y &&
-    sudo dnf install git ripgrep fd-find gcc hub neovim python3-neovim shell-color-scripts -y,
+    sudo dnf install git ripgrep fd-find gcc hub neovim python3-neovim shell-color-scripts -y
 }
 
 install_neovim
